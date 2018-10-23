e949c4ef2ad7c70d65f3434a04f16dc2af9a331b
Nima Izadi <nima.izadi@shopify.com>
2018-10-23 09:17:07 -0400

    Fix tests (#231)
    * Added integrety sha to yarn.lock
    
    * Fixed tests
    
    * Removed duplicates

--- scripts/tests/utils.js
+++ scripts/tests/utils.js
@@ -1,14 +1,18 @@
 export function getDuplicates(conferences) {
-  const confUUIDs = conferences.map((conf) => `${conf.url}-${conf.city}`);
+  const confUUIDs = conferences.map((conf) => getUUID(conf));
   const duplicates = [];
 
   Object.keys(conferences).forEach((key, index) => {
-    const url = conferences[key].url;
-    if (confUUIDs.indexOf(url, index + 1) !== -1) {
-      if (duplicates.indexOf(url) === -1) {
+    const uuid = getUUID(conferences[key]);
+    if (confUUIDs.indexOf(uuid, index + 1) !== -1) {
+      if (duplicates.indexOf(uuid) === -1) {
         duplicates.push(conferences[key]);
       }
     }
   });
   return duplicates;
+}
+
+function getUUID(conf) {
+  return `${conf.url.replace(/https?:\/\//, '')}-${conf.city}`;
 }
