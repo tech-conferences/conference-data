617e9f0bb4332e00d9de7cf8490059c25cd73d31
Christian Grail <christian.grail@sap.com>
2020-04-27 15:45:46 +0200

    Extract checkConferences

--- scripts/tests/conferences.test.js
+++ scripts/tests/conferences.test.js
@@ -1,52 +1,26 @@
 const conferenceReader = require('../conferenceReader');
-const checkConference = require('./utils/checkConference');
-const getDuplicates = require('./utils/getDuplicates');
+const checkConferences = require('./utils/checkConferences');
 const logTestResult = require('./utils/logTestResult');
-const findLineNumber = require('./utils/findLineNumber');
 
 const label = 'Conference Tests Runtime';
 console.time(label);
 
 const conferencesJSON = conferenceReader();
 
-const testResult = {};
-var conferenceCounter = 0;
+const testResult = {
+    errors: {},
+    conferenceCounter: 0
+};
 
 for (const year of Object.keys(conferencesJSON)) {
-    testResult[year] = {};
+    testResult.errors[year] = {};
     for (const stack of Object.keys(conferencesJSON[year])) {
-        const currentErrors = [];
-        testResult[year][stack] = currentErrors;
         const conferences = conferencesJSON[year][stack];
-        const fileName = `conferences/${year}/${stack}.json`;
+        testResult.errors[year][stack] = checkConferences(year, stack, conferences);
+        testResult.conferenceCounter += conferences.length;
 
-        function reportError(lineNumber, message, value) {
-            currentErrors.push({
-                fileName: fileName,
-                lineNumber: lineNumber,
-                message: message,
-                value: value
-            });
-        }
-
-        const duplicates = getDuplicates(conferences);
-        if (duplicates.length > 0) {
-            const lineNumber = findLineNumber(duplicates[0], 'name', fileName);
-            reportError(lineNumber, `Found duplicate conference "${duplicates.map(conf => conf.name)}"`);
-        }
-
-        for (const conference of conferences) {
-            conferenceCounter++;
-            function assertField(condition, field, message, value) {
-                if (!condition) {
-                    const lineNumber = findLineNumber(conference, field, fileName);
-                    reportError(lineNumber, `[${field}] ${message}`, value);
-                }
-            }
-            checkConference(year, conference, assertField);
-        };
     };
 };
 
-logTestResult(testResult, conferenceCounter);
+logTestResult(testResult);
 console.timeEnd(label);
